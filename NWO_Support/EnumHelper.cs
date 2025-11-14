8bb063dc004fd2404527a951df729bdac429cbaf
Vladislav Smirnov <vladislav.smirnov@pix-it.ru>
2025-11-14 15:16:17 +0300

    14_11_2025 - Подготовлены: enum UnitBehaviorType и EnumHlper

---
+++ NWO_Support/EnumHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace NWO_Support
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            if (field is null)
+                return string.Empty;
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
